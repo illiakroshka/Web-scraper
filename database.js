ac84581fd0dba9af47b605857b3b9cdbc3b17fb7
Illia <kroshka.illya@lll.kpi.ua>
2023-04-25 15:33:09 +0300

    Implemente JSON database with functions

---
+++ database.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+
+const createDataBase = () => {
+  const db = {
+    Comfy: [],
+    Allo: [],
+  };
+  const content = JSON.stringify(db);
+  fs.writeFileSync('./database.json', content);
+}
+
+const getData = () => {
+  if (!fs.existsSync('./database.json')) createDataBase();
+  const content = fs.readFileSync('./database.json', 'utf-8');
+  const data = JSON.parse(content);
+  return data; 
+}
+
+const writeComfyValue = (data) => {
+  const db = getData();
+  db.Comfy.push(data)
+  const content = JSON.stringify(db);
+  fs.writeFileSync('./database.json',content)
+}
+
+const writeAlloValue = (data) => {
+  const db = getData();
+  db.Allo.push(data)
+  const content = JSON.stringify(db);
+  fs.writeFileSync('./database.json',content)
+}
