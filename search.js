744ddd8d32594fb36dbf77e816dac18b515a3741
Illia <kroshka.illya@lll.kpi.ua>
2023-05-10 16:28:11 +0300

    refactor search.js

--- search.js
+++ search.js
@@ -2,16 +2,16 @@
 
 const database = require('./database');
 
-const content = database.getData();
-
-const comfySearch = (name, data = content) => {
+const comfySearch = (name) => {
+  const data = database.getData();
   for (const array of data.Comfy) {
     let laptop = array.find(product => product.name.includes(name));
     if (laptop) return laptop;
   }
 }
 
-const alloSearch = (name, data = content) => {
+const alloSearch = (name) => {
+  const data = database.getData();
   for (const array of data.Allo) {
     let laptop = array.find(product => product.name.includes(name));
     if (laptop) return laptop;
